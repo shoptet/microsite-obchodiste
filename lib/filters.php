87d07e2c1c72b2f61b71ff4fa47e61e0d77aafc3
Jakub Kolář <jk.oolar@gmailcom>
2018-08-06 10:15:02 +0200

    Fix code formating

--- lib/filters.php
+++ lib/filters.php
@@ -6,7 +6,7 @@
 add_filter( 'add_meta_boxes', function() {
    // Hide category meta box
   remove_meta_box( 'tagsdiv-wholesaler_category', 'custom', 'side' );
-	remove_meta_box( 'wholesaler_categorydiv', 'custom', 'side' ); // if taxonomy is hierarchical
+  remove_meta_box( 'wholesaler_categorydiv', 'custom', 'side' ); // if taxonomy is hierarchical
    // Hide featured image metabox
   remove_meta_box( 'postimagediv', 'custom', 'side' );
 } );
@@ -17,9 +17,9 @@ add_filter( 'add_meta_boxes', function() {
  */
 add_filter( 'acf/update_value/name=logo', function( $value, $post_id, $field ) {
   // Not the correct post type, bail out
-	if ( 'custom' !== get_post_type( $post_id ) ) {
-		return $value;
-	}
+  if ( 'custom' !== get_post_type( $post_id ) ) {
+    return $value;
+  }
   // Skip empty value
   if ( $value != ''  ) {
     // Add the value which is the image ID to the _thumbnail_id meta data for the current post
