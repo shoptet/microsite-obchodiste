527c824fecea56f97b9eb3fb14a0a3e805eabda9
Jakub Kolář <jk.oolar@gmailcom>
2018-08-23 20:37:25 +0200

    Add urls to header links

--- template-parts/page/header-custom.php
+++ template-parts/page/header-custom.php
@@ -1,4 +1,4 @@
 <div class="login header-login">
-    <a href="#" class="btn btn-orange btn-add has-icon"><i class="fas fa-plus-circle"></i>Přidat velkoobchod</a>
-    <a class="has-icon" href="#"><i class="fas fa-user"></i> <span>Přihlášení</span></a>
+    <a href="<?php echo admin_url( 'post-new.php?post_type=custom' ); ?>" class="btn btn-orange btn-add">Přidat velkoobchod</a>
+    <a class="has-icon" href="<?php echo wp_login_url(); ?>"><i class="fas fa-user"></i> <span>Přihlášení</span></a>
 </div>
