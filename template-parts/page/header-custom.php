5668129a625b6bb22b591e7f1e669f30f51a28ee
Jakub Kolář <jk.oolar@gmailcom>
2018-08-24 11:50:45 +0200

    Add dropdown menu to logged in user

--- template-parts/page/header-custom.php
+++ template-parts/page/header-custom.php
@@ -1,4 +1,38 @@
 <div class="login header-login">
-    <a href="<?php echo admin_url( 'post-new.php?post_type=custom' ); ?>" class="btn btn-orange btn-add">Přidat velkoobchod</a>
-    <a class="has-icon" href="<?php echo wp_login_url(); ?>"><i class="fas fa-user"></i> <span>Přihlášení</span></a>
+    <a href="<?php echo admin_url( 'post-new.php?post_type=custom' ); ?>" class="btn btn-orange btn-add">
+      <?php _e( 'Přidat velkoobchod', '' ); ?>
+    </a>
+    <?php if ( is_user_logged_in() ): ?>
+      <span class="dropdown">
+        <a
+          class="align-middle py-2 ml-sm-3"
+          role="button"
+          href="<?php echo admin_url(); ?>"
+          id="dropdownMenuButton"
+          data-toggle="dropdown"
+          aria-haspopup="true"
+          aria-expanded="false"
+        >
+          <?php echo wp_get_current_user()->user_email; ?>
+          <i class="fas fa-angle-down" aria-hidden="true"></i>
+        </a>
+        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="dropdownMenuButton">
+          <a class="dropdown-item" href="<?php echo admin_url( 'edit.php?post_type=custom' ); ?>">
+            <?php _e( 'Moje velkoobchody', '' ); ?>
+          </a>
+          <a class="dropdown-item" href="<?php echo admin_url( 'profile.php' ); ?>">
+            <?php _e( 'Můj profil', '' ); ?>
+          </a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="<?php echo wp_logout_url(); ?>">
+            <?php _e( 'Odhlásit', '' ); ?>
+          </a>
+        </div>
+      </span>
+    <?php else: ?>
+      <a class="has-icon" href="<?php echo wp_login_url(); ?>">
+        <i class="fas fa-user"></i>
+        <span><?php _e( 'Přihlášení', '' ); ?></span>
+      </a>
+    <?php endif; ?>
 </div>
