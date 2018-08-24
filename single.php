7010e40f27e5e1144802436f6fcfe1e652a96b4a
Jakub Kolar <jk.oolar@gmail.com>
2018-08-24 06:45:59 +0200

    Add related wholesalers to detail (#7)

--- single.php
+++ single.php
@@ -10,10 +10,7 @@
 
         get_template_part( 'src/template-parts/wholesaler/content', 'content' );
 
-        // If comments are open or we have at least one comment, load up the comment template.
-        if ( comments_open() || get_comments_number() ) :
-        	comments_template();
-        endif;
+        get_template_part( 'src/template-parts/wholesaler/content', 'related' );
 
         endwhile; // End of the loop.
     ?>
---
+++ template-parts/wholesaler/content-related.php
@@ -0,0 +1,34 @@
+<?php
+$terms = get_the_terms( $post->ID, 'customtaxonomy' );
+$term_ids = array_map( function( $term ) {
+  return $term->term_id;
+}, $terms );
+$the_query = new WP_Query( [
+  'post_type' => 'custom',
+  'posts_per_page' => 2,
+  'post_status' => 'publish',
+  'post__not_in' => [ $post->ID ], // exclude current post
+  'tax_query' => [ [
+    'taxonomy' => 'customtaxonomy',
+    'field' => 'term_id',
+    'terms' => $term_ids,
+  ] ],
+] );
+?>
+<?php if ( $the_query->post_count ): ?>
+  <div class="pt-5 pb-4">
+    <h2 class="text-center h3 mb-1">
+      <?php _e( 'Nezaujal vás tento dodavatel?', '' ); ?>
+    </h2>
+    <p class="mb-4 text-center mb-5">
+      <?php _e( 'Podívejte se na další ze stejné kategorie', '' ); ?>
+    </p>
+    <div class="row row-bordered no-gutters">
+      <?php while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
+        <div class="col-12 col-lg-6">
+          <?php get_template_part( 'src/template-parts/wholesaler/content', 'tease' ); ?>
+        </div>
+      <?php endwhile; ?>
+    </div>
+  </div>
+<?php endif ?>
