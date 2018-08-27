bb5028b91eaa32b94c7858e6c3dc4f15aedba940
Jakub Kolář <jk.oolar@gmailcom>
2018-08-27 11:30:26 +0200

    Enlarge wholesaler tease logo

--- template-parts/wholesaler/content-tease.php
+++ template-parts/wholesaler/content-tease.php
@@ -3,9 +3,9 @@
   href="<?php the_permalink(); ?>"
   title="<?php _e( 'Zobrazit profil', '' ); ?>"
 >
-  <div class="d-flex align-items-center">
+  <div class="d-flex">
 
-    <div class="wholesaler-tease-logo mr-3 <?php if ( ! get_field( "logo" ) ) echo "wholesaler-tease-logo-empty" ?>">
+    <div class="wholesaler-tease-logo flex-shrink-0 mr-3 <?php if ( ! get_field( "logo" ) ) echo "wholesaler-tease-logo-empty" ?>">
       <?php if ( get_field( "logo" ) ): ?>
       <img
         src="<?php echo get_field( "logo" )[ "sizes" ][ "medium" ]; ?>"
@@ -14,24 +14,28 @@
       <?php endif; ?>
     </div>
 
-    <div>
+    <div class="mt-1">
 
       <h3 class="wholesaler-tease-title h5 mb-2">
         <?php the_title(); ?>
       </h3>
 
-      <?php $terms = get_the_terms( $post->ID, 'customtaxonomy' ); ?>
-      <?php if ( ! empty( $terms ) ):  ?>
       <p class="fs-90 mb-0">
-        <?php foreach ( $terms as $term ): ?>
-        <?php echo $term->name; ?>
-        <?php endforeach; ?>
-      </p>
-      <?php endif; ?>
+        <?php $terms = get_the_terms( $post->ID, 'customtaxonomy' ); ?>
+        <?php if ( ! empty( $terms ) ):  ?>
+          <?php foreach ( $terms as $term ): ?>
+          <?php echo $term->name; ?>,
+          <?php endforeach; ?>
+        <?php endif; ?>
 
-      <?php if ( get_field( "region" ) ): ?>
-      <p class="fs-90 mb-0">
-        <?php echo get_field( "region" )['label']; ?>
+        <?php if ( get_field( "region" ) ): ?>
+          <?php echo get_field( "region" )['label']; ?>
+        <?php endif; ?>
+      </p>
+
+      <?php if ( get_field( "short_about" ) ): ?>
+      <p class="wholesaler-tease-description fs-90 fs-lg-100 mt-2 mb-0">
+        <?php echo truncate( strip_tags( get_field( "short_about" ) ), 110 ); ?>
       </p>
       <?php endif; ?>
 
@@ -49,9 +53,4 @@
   </div>
   <?php endif;  ?>
 
-  <?php if ( get_field( "short_about" ) ): ?>
-  <p class="wholesaler-tease-description mt-2 mb-0">
-    <?php echo truncate( strip_tags( get_field( "short_about" ) ), 110 ); ?>
-  </p>
-  <?php endif; ?>
 </a>
