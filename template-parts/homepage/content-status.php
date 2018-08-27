5240deffb49e3d5765e6976d256952aae8c6836f
Jakub Kolář <jk.oolar@gmailcom>
2018-08-27 09:31:34 +0200

    Update homepage status row

--- template-parts/homepage/content-status.php
+++ template-parts/homepage/content-status.php
@@ -3,10 +3,10 @@
     <p class="h3 mb-0">
       <?php
       printf(
-        __( '<strong>%d</strong>&nbsp;velkoobchodů z&nbsp;<strong>%d</strong>&nbsp;kategorií a&nbsp;<strong>%d</strong>&nbsp;krajů', '' ),
+        __( '<strong>%d</strong>&nbsp;velkoobchodů z&nbsp;<strong>%d</strong>&nbsp;kategorií obdrželo poptávky od <strong>%d</strong>&nbsp;e-shopů', '' ),
         wp_count_posts( 'custom' )->publish,
         count( get_terms( 'customtaxonomy' ) ),
-        count( get_used_regions() )
+        wp_count_posts( 'wholesaler_message' )->publish
       );
       ?>
     </p>
