b9e0e88871f7ba846db87172c50031adc9f5fcea
Jakub Kolář <jk.oolar@gmailcom>
2018-08-27 18:06:50 +0200

    Add more rows at contact form textarea

--- template-parts/wholesaler/content-contact-form.php
+++ template-parts/wholesaler/content-contact-form.php
@@ -24,7 +24,7 @@
       <label class="required-asterisk" for="wholesalerContactFormMessage">
         <?php _e( 'Zpráva velkoobchodu', '' ); ?>
       </label>
-      <textarea class="form-control" name="message" rows="5" id="wholesalerContactFormMessage" required></textarea>
+      <textarea class="form-control" name="message" rows="9" id="wholesalerContactFormMessage" required></textarea>
     </div>
 
     <input type="hidden" name="wholesaler_id" value="<?php the_ID(); ?>">
