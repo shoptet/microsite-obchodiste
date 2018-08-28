bed61980a749f82ecc0ca8d547d8b75d72dfe31e
Jakub Kolar <jk.oolar@gmail.com>
2018-08-28 14:39:20 +0200

    Add banner to wholesaler archive (#9)

--- template-parts/wholesaler/content-archive.php
+++ template-parts/wholesaler/content-archive.php
@@ -31,12 +31,26 @@
         <?php get_template_part( 'src/template-parts/wholesaler/content', 'archive-sort' ); ?>
 
         <div class="row row-bordered no-gutters">
+          <?php $loop_index = 0; ?>
           <?php while ( have_posts() ) : the_post(); ?>
             <div class="col-12 col-lg-6">
 
               <?php get_template_part( 'src/template-parts/wholesaler/content', 'tease' ); ?>
 
             </div>
+
+            <?php if ( $loop_index === 5 && get_fields( 'options' )[ 'archive_post' ] ): ?>
+              </div>
+              <div class="row">
+                <div class="col-12 col-lg-10 offset-lg-1 col-xl-8 offset-xl-2 py-4">
+                  <?php get_template_part( 'src/template-parts/post/content', 'banner' ); ?>
+                </div>
+              </div>
+              <div class="row row-bordered no-gutters">
+            <?php endif; ?>
+
+            <?php $loop_index++; ?>
+
           <?php endwhile; ?>
         </div>
 
