a9533631043eae61e0a39e59c344548141291c30
Jakub Kolář <jk.oolar@gmailcom>
2019-01-17 11:20:56 +0100

    Special offer carousel autoplay

--- _js/main.js
+++ _js/main.js
@@ -9,8 +9,12 @@ $(function() {
   });
 
   $('.owl-carousel').owlCarousel({
+    loop: true,
     nav: true,
     dots: false,
+    autoplay: true,
+    autoplayTimeout: 5000,
+    autoplayHoverPause: true,
     responsive: {
       0:    { items: 1 },
       576:  { items: 1 },
