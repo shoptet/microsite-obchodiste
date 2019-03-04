f7ffde6f52bdb54a62193720d38a332db71e8cd4
Jakub Kolář <jk.oolar@gmailcom>
2019-03-04 18:30:15 +0100

    Add product detail template

---
+++ template-parts/product/content-content.php
@@ -0,0 +1,42 @@
+<div class="row wholesaler" itemscope itemtype="http://schema.org/Organization">
+  <div class="col-12 col-lg-7 col-xl-8">
+
+    <?php get_template_part( 'src/template-parts/product/content', 'header' ); ?>
+
+    <?php if ( get_field( "description" ) ): ?>
+    <div>
+      <h2 class="h-heavy mb-1">
+        <?php _e( 'Popis produktu', 'shp-obchodiste' ); ?>
+      </h2>
+      <div>
+        <?php the_field( "description" ); ?>
+      </div>
+    </div>
+    <?php endif; ?>
+
+  </div>
+  <div class="col-12 col-lg-5 col-xl-4">
+
+    <?php
+      if ( $related_wholesaler = get_field( "related_wholesaler" ) ):
+      // Set global post variable to related wholesaler post
+      global $post; 
+      $post = get_post( $related_wholesaler->ID );
+      setup_postdata( $post );
+    ?>
+
+    <?php get_template_part( 'src/template-parts/wholesaler/content', 'contact-company' ); ?>
+
+    <?php get_template_part( 'src/template-parts/wholesaler/content', 'contact-person' ); ?>
+
+    <?php get_template_part( 'src/template-parts/wholesaler/content', 'contact-form' ); ?>
+
+    <?php
+      wp_reset_postdata();
+      endif;
+    ?>
+
+  </div>
+</div>
+
+<?php get_template_part( 'src/template-parts/wholesaler/content', 'special-offers' ); ?>
