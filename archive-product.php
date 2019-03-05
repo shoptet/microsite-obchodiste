f1e2ed5cdbfd6b7ff1adc18593f47908a6d0cd52
Jakub Kolář <jk.oolar@gmailcom>
2019-03-05 22:15:42 +0100

    Add product archive template

---
+++ archive-product.php
@@ -0,0 +1,9 @@
+<?php get_header(); ?>
+
+<section class="section section-primary">
+  <div class="section-inner container">
+    <?php get_template_part( 'src/template-parts/product/content', 'archive' ); ?>
+  </div>
+</section>
+
+<?php get_footer(); ?>
