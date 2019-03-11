5fb19a70b76add67dbc554b88c32280ee5ec648b
Jakub Kolář <jk.oolar@gmailcom>
2019-03-11 14:33:24 +0100

    Add facade navigation

--- archive-special_offer.php
+++ archive-special_offer.php
@@ -1,10 +1,9 @@
 <?php get_header(); ?>
 
 <section class="section section-primary">
-    <div class="section-inner container">
-    <?php get_template_part( 'src/template-parts/special_offer/content', 'archive' ); ?>
-
-    </div>
+  <div class="section-inner container">
+    <?php get_template_part( 'src/template-parts/common/content', 'archive' ); ?>
+  </div>
 </section>
 
 <?php get_footer(); ?>
