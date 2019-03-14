a31cc28a2519f53b248c4dfce6496b1f1c896812
Jakub Kolář <jk.oolar@gmailcom>
2019-03-14 15:05:36 +0100

    Add related products to wholesaler detail

--- template-parts/common/content-archive.php
+++ template-parts/common/content-archive.php
@@ -59,24 +59,8 @@ switch ( $post_type ) {
         </div>
         <?php elseif ($post_type === 'product'): ?>
         <div class="list-bordered">
-          <div class="list-bordered-item d-none d-lg-block bg-light font-weight-bold py-2 px-3">
-            <div class="d-flex">
-              <div class="flex-shrink-0 mr-3">
-                <div class="product-tease-image-placeholder"></div>
-              </div>
-              <div class="row w-100">
-                <div class="col-12 col-lg-6">
-                  <?php _e( 'Název a popis produktu', 'shp-obchodiste' ); ?>
-                </div>
-                <div class="col-12 col-lg-3 text-center">
-                  <?php _e( 'Velkoobchod', 'shp-obchodiste' ); ?>
-                </div>
-                <div class="col-12 col-lg-3 text-right">
-                  <?php _e( 'Cena', 'shp-obchodiste' ); ?>
-                </div>
-              </div>
-            </div>
-          </div>
+          <?php get_template_part( 'src/template-parts/product/content', 'list-header' ); ?>
+          
           <?php $GLOBALS[ 'is_product_tease_in_row' ] = true; ?>
           <?php while ( have_posts() ) : the_post(); ?>
             <div class="list-bordered-item">
