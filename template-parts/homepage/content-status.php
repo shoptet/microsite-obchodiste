2a2460e574a9d21a28eb08b0c62ee892f75d31ce
Jakub Kolář <jk.oolar@gmailcom>
2019-04-18 11:27:29 +0200

    Update status text on homepage

--- template-parts/homepage/content-status.php
+++ template-parts/homepage/content-status.php
@@ -5,9 +5,9 @@
       $options = get_fields( 'options' );
       $fake_message_number = ( isset( $options[ 'fake_message_number' ] ) ) ? (int) $options[ 'fake_message_number' ] : 0;
       printf(
-        __( '<strong>%d</strong>&nbsp;velkoobchodů z&nbsp;<strong>%d</strong>&nbsp;kategorií obdrželo <strong>%d</strong>&nbsp;poptávek od e-shopů', 'shp-obchodiste' ),
+        __( '<strong>%d</strong>&nbsp;velkoobchodů s&nbsp;nabídkou <strong>%d</strong>&nbsp;produktů obdrželo <strong>%d</strong>&nbsp;poptávek od e-shopů', 'shp-obchodiste' ),
         wp_count_posts( 'custom' )->publish,
-        count( get_terms( 'customtaxonomy' ) ),
+        wp_count_posts( 'product' )->publish,
        ( wp_count_posts( 'wholesaler_message' )->publish + $fake_message_number )
       );
       ?>
