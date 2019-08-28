546aa55cebf0269879109c29450385d3e7e7bae7
Jakub Kolář <jk.oolar@gmailcom>
2019-08-28 15:38:00 +0200

    Add tree structure

--- template-parts/common/content-archive-filter-terms-product.php
+++ template-parts/common/content-archive-filter-terms-product.php
@@ -4,31 +4,28 @@ $post_type = get_post_type_in_archive_or_taxonomy();
 $taxonomy = $post_type . 'taxonomy';
 $current_term = ( is_tax() ? $wp_query->get_queried_object() : null );
 
-$render_branch = function ( $parent_term = null ) use ( &$render_branch, &$taxonomy, &$current_term, &$post_type ) {
+$render_tree = function ( $parent_term = null ) use ( &$render_tree, &$taxonomy, &$current_term, &$post_type ) {
   $terms = get_terms( [
     'taxonomy' => $taxonomy,
     'parent' => $parent_term ? $parent_term->term_id : 0,
+    'hide_empty' => true,
   ] );
+  if ( empty( $terms ) ) return;
   ?>
-  <ul>
+  <ul class="<?php echo ( $parent_term ? 'list-tree' : 'list-unstyled' ); ?>">
     <?php foreach ( $terms as $term ): ?>
       <?php
-      $is_parent = $current_term && term_is_ancestor_of( $term, $current_term, $taxonomy );
       $is_current = $current_term && $term->term_id == $current_term->term_id;
+      $is_parent = $current_term && term_is_ancestor_of( $term, $current_term, $taxonomy );
       $posts_in_term = count_posts_by_term( $post_type, $term, $taxonomy );
       ?>
       <li>
         <a href="<?php echo get_term_link( $term, $taxonomy ); ?>">
-          <span
-            class="<?php echo ( $is_current ? 'font-weight-bold' : '' ); ?>"
-          >
-            <?php echo $term->name; ?>
-          </span>
-          <span class="text-semilight">(<?php echo $posts_in_term; ?>)</span>
+          <span class="<?php echo ( $is_current ? 'font-weight-bold' : '' ); ?>"><?php echo $term->name; ?></span>&nbsp;<span class="text-semilight">(<?php echo $posts_in_term; ?>)</span>
         </a>
         <?php
         if ( $is_current || $is_parent ) {
-          $render_branch( $term );
+          $render_tree( $term );
         }
         ?>
       </li>
@@ -42,6 +39,8 @@ $render_branch = function ( $parent_term = null ) use ( &$render_branch, &$taxon
   <?php _e( 'Kategorie', 'shp-obchodiste' ); ?>
 </p>
 
-<?php $render_branch(); ?>
+<div class="filters-list">
+  <?php $render_tree(); ?>
+</div>
 
 <div class="filters-divider"></div>
