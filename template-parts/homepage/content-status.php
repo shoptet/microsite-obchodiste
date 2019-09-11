8d2e1f9212a41eeea5b6e8ffad61aa6b771ad32e
Jakub Kolář <jk.oolar@gmailcom>
2019-09-11 10:56:13 +0200

    Update status row on homepage

--- template-parts/homepage/content-status.php
+++ template-parts/homepage/content-status.php
@@ -5,9 +5,8 @@
       $options = get_fields( 'options' );
       $fake_message_number = ( isset( $options[ 'fake_message_number' ] ) ) ? (int) $options[ 'fake_message_number' ] : 0;
       printf(
-        __( '<strong>%d</strong>&nbsp;velkoobchodů s&nbsp;nabídkou <strong>%d</strong>&nbsp;produktů obdrželo <strong>%d</strong>&nbsp;poptávek od e-shopů', 'shp-obchodiste' ),
+        __( '<strong>%d</strong>&nbsp;velkoobchodů obdrželo <strong>%d</strong>&nbsp;poptávek od e-shopů', 'shp-obchodiste' ),
         wp_count_posts( 'custom' )->publish,
-        wp_count_posts( 'product' )->publish,
        ( wp_count_posts( 'wholesaler_message' )->publish + $fake_message_number )
       );
       ?>
