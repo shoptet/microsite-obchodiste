f38cae3119fdd4023124f43e48baf3a4e230fad2
Jakub Kolář <jk.oolar@gmailcom>
2020-04-28 22:22:29 +0200

    Add success massage

--- _js/wholesaler-autofill.js
+++ _js/wholesaler-autofill.js
@@ -8,28 +8,24 @@
 
   function init() {
     $button = $('<input class="button button-primary button-large" type="button">')
-    .attr('value', local.button_label)
-    .css({
-      'margin-top': '1rem',
-    });
-    $error = $('<div></div>')
-      .css({
-        'color': '#f00',
-        'margin-top': '0.5rem',
-      });
+      .attr('value', local.button_label)
+      .css('margin-top', '1rem');
+    $message = $('<div></div>')
+      .css('margin-top', '0.5rem');
     $autofill = $('<div></div>')
       .append($button)
-      .append($error);
+      .append($message);
     $autofillContainer = $('[data-key=field_5b5ecaf4052fb] .acf-input');
 
-    updateAutofillAbility();
-    updateButtonAbility();
+    updateAutofillEnability();
+    updateButtonEnability();
 
-    $('#acf-field_5bbdc26030686').on('change', updateAutofillAbility);
-    $('#acf-field_5b5ecaf4052fb').on('input', updateButtonAbility);
+    $('#acf-field_5bbdc26030686').on('change', updateAutofillEnability);
+    $('#acf-field_5b5ecaf4052fb').on('input', updateButtonEnability);
   }
 
   function fillForm(data) {
+    //$('#title').val(data.name);
     $('#acf-field_5b5ecc9d052fc').val(data.tin);
     $('#acf-field_5b5ec9b4052f8').val(data.street);
     $('#acf-field_5b5eca63052f9').val(data.city);
@@ -37,20 +33,20 @@
     $('#acf-field_5b5ed2ca0a22d').val(data.region);
   };
 
-  function showError(message) {
-    if (typeof message === 'string' && message.length > 0) {
-      $error.html(message);
-    } else {
-      $error.html(local.error);
+  function showMessage(type, message) {
+    if (typeof message !== 'string' || message.length == 0) {
+      message = local[type];
     }
+    var color = (type == 'success' ? '#46b450' : '#dc3232');
+    $message.html('<div style="color:' + color + '">' + message + '</div>');
   }
 
-  function clearError() {
-    $error.html('');
+  function clearMessage() {
+    $message.html('');
   }
-  
+
   function handleAutofill() {
-    clearError();
+    clearMessage();
     $button.attr('disabled', true);
 
     var data = {
@@ -64,23 +60,24 @@
       .done(function(res) {
         if ( res.success == true ) {
           fillForm(res.data);
+          showMessage('success');
         } else {
-          showError(res.data);
+          showMessage('error', res.data);
         }
       })
       .always(function() {
         $button.attr('disabled', false);
       })
       .fail(function() {
-        showError();
+        showMessage('error');
       });
   }
 
   function enableAutofill() {
     if (!enabled) {
-      clearError();
+      clearMessage();
       $autofillContainer.append($autofill);
-      updateButtonAbility();
+      updateButtonEnability();
       $button.on('click', handleAutofill);
       enabled = true;
     }
@@ -94,7 +91,7 @@
     }
   }
 
-  function updateAutofillAbility() {
+  function updateAutofillEnability() {
     var country = $('#acf-field_5bbdc26030686').val();
     if ('cz' == country) {
       enableAutofill();
@@ -103,7 +100,7 @@
     }
   }
 
-  function updateButtonAbility() {
+  function updateButtonEnability() {
     var inVal = $('#acf-field_5b5ecaf4052fb').val();
     $button.attr('disabled', !inVal.length);
   }
