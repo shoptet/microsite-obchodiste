60d47bff7c536e9a7923bc030ec6721740e2bcde
Jakub Kolář <jk.oolar@gmailcom>
2020-06-02 14:32:10 +0200

    Update text on homepage

--- template-parts/homepage/content-status.php
+++ template-parts/homepage/content-status.php
@@ -2,6 +2,7 @@
 $options = get_fields( 'options' );
 $fake_message_number = ( isset( $options[ 'fake_message_number' ] ) ) ? (int) $options[ 'fake_message_number' ] : 0;
 $custom_post_count = CounterCache::getPostTypeCount( 'custom' );
+$product_post_count = CounterCache::getPostTypeCount( 'product' );
 $message_post_count = CounterCache::getPostTypeCount( 'wholesaler_message' );
 ?>
 <div class="row-status">
@@ -9,8 +10,9 @@ $message_post_count = CounterCache::getPostTypeCount( 'wholesaler_message' );
     <p class="h3 mb-0">
       <?php
       printf(
-        __( '<strong>%d</strong>&nbsp;velkoobchodů obdrželo <strong>%d</strong>&nbsp;poptávek od e-shopů', 'shp-obchodiste' ),
+        __( '<strong>%d</strong>&nbsp;velkoobchodů s&nbsp;nabídkou <strong>%d</strong>&nbsp;produktů obdrželo <strong>%d</strong>&nbsp;poptávek od e-shopů', 'shp-obchodiste' ),
         $custom_post_count,
+        $product_post_count,
         ( $message_post_count + $fake_message_number )
       );
       ?>
