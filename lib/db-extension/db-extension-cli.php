017f1ddb38c6f92d322039335b3f1f7f43cd5b78
Jakub Kolář <jk.oolar@gmailcom>
2020-08-14 14:53:39 +0200

    Ability to migrate only one meta key via WP CLI

--- lib/db-extension/db-extension-cli.php
+++ lib/db-extension/db-extension-cli.php
@@ -37,10 +37,116 @@ class DBXCli {
       \WP_CLI::error( 'Post type is not registered!' );
     }
 
-    // TODO: migrate only one meta key
-    $meta_key = '';
-    if ( !empty($assoc_args['meta-key']) && 'true' == $assoc_args['meta-key'] ) {
-      $meta_key = $assoc_args['meta-key'];
+    if ( empty($args[1]) ) {
+      \WP_CLI::error( 'Meta key is required!' );
+    }
+    $meta_key = $args[1];
+
+    $dbx_post_type = $dbx->get_registered_post_type($post_type);
+    $extended_meta_keys = $dbx_post_type->get_extended_meta_keys($post_type);
+
+    if ( !in_array($meta_key, $extended_meta_keys) ) {
+      \WP_CLI::error( 'Meta key is not registered!' );
+    }
+    
+    // Let the user know in what mode the command runs.
+    if ( $dry_run ) {
+      \WP_CLI::log( 'Running in dry-run mode.' );
+    } else {
+      \WP_CLI::log( 'We\'re doing it live!' );
+    }
+
+    $posts_per_page = 350;
+    $paged = 1;
+    $migrated_posts = 0;
+    
+    $args = [
+      'post_type' => $post_type,
+      'post_status' => 'any',
+      'fields' => 'ids',
+      'posts_per_page' => $posts_per_page,
+      'update_post_meta_cache' => false,
+      'update_post_term_cache' => false,
+    ];
+
+    $query = new \WP_Query( $args );
+
+    $progress = \WP_CLI\Utils\make_progress_bar( 'Migrating posts', $query->found_posts );
+
+    do {
+      $args['paged'] = $paged;
+      $query = new \WP_Query( $args );
+
+      foreach ( $query->posts as $post_id ) {
+
+        if ( $dbx_post_type->get_store()->row_exists($post_id) ) {
+          if ( empty(get_post_meta($post_id, $meta_key)) ) {
+            $meta_data = DBXUtility::get_original_meta_data($post_id);
+            if ( array_key_exists( $meta_key, $meta_data )) {
+              if ( ! $dry_run ) {
+                update_post_meta( $post_id, $meta_key, $meta_data[$meta_key] );
+              }
+            }
+            $migrated_posts++;
+          } else {
+            // Meta key for post exists
+          }
+        } else {
+          \WP_CLI::log( sprintf( 'Row for post %d does not exists', $post_id ) );
+        }
+
+        if ( $remove_original_meta ) {
+          if ( ! $dry_run ) {
+            DBXUtility::delete_original_meta_data($post_id, [ $meta_key ]);
+          } 
+        }
+
+      }
+
+      // Free up memory.
+      stop_the_insanity();
+
+      $progress->tick( count($query->posts) );
+      $paged++;
+
+    } while ( count($query->posts) );
+
+    $progress->finish();
+
+    end_bulk_operation();
+
+    if ( $dry_run ) {
+      \WP_CLI::success( sprintf( '%d posts will be migrated', $migrated_posts) );
+    } else {
+      \WP_CLI::success( sprintf( '%d posts have been migrated', $migrated_posts) );
+    }
+
+  }
+
+  public function migrateall( $args, $assoc_args ) {
+
+    start_bulk_operation();
+
+    wp_cache_flush();
+
+    $dry_run = false;
+    if ( !empty($assoc_args['dry-run']) && 'true' == $assoc_args['dry-run'] ) {
+      $dry_run = true;
+    }
+
+    $remove_original_meta = false;
+    if ( !empty($assoc_args['remove-original']) && 'true' == $assoc_args['remove-original'] ) {
+      $remove_original_meta = true;
+    }
+    
+    if ( empty($args[0]) ) {
+      \WP_CLI::error( 'Post type is required!' );
+    }
+    $post_type = $args[0];
+
+    global $dbx;
+    if ( ! in_array( $post_type, $dbx->get_registered_post_types() ) ) {
+      \WP_CLI::error( 'Post type is not registered!' );
     }
 
     $dbx_post_type = $dbx->get_registered_post_type($post_type);
